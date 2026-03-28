e94412fb275110b7cb84a05274efdfdf6611c5a9
thavao <othavioact@gmail.com>
2026-03-28 14:45:22 -0300

    altera transaction

--- src/Fin.Domain/Entities/Transaction.cs
+++ src/Fin.Domain/Entities/Transaction.cs
@@ -6,12 +6,13 @@ public class Transaction
 {
     public required int Id { get; set; }
     public required string Title { get; set; }
-    public required string Description { get; set; }
+    public string? Description { get; set; }
     public required decimal Amoount { get; set; }
     public DateTime TransactionDate { get; set; }
     public int Installment { get; set; }
     public int InstallmentCount { get; set; }
     public required Wallet Wallet { get; set; } = new Wallet();
+    public int WalletId { get; set; }
     public required bool IsRecurring { get; set; }
     public required TransactionType Type { get; set; }
     public required DateTime DueDate { get; set; }
