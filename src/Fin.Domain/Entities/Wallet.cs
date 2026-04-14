851fc80384e80771a0519d0d44cc01a2ea843544
thavao <othavioact@gmail.com>
2026-04-13 21:19:59 -0300

    feature: add propriedade name a wallet

--- src/Fin.Domain/Entities/Wallet.cs
+++ src/Fin.Domain/Entities/Wallet.cs
@@ -3,6 +3,7 @@ namespace Fin.Domain.Entities;
 public class Wallet
 {
     public int Id { get; set; }
+    public string Name { get; set; }
     public decimal Limit { get; set; }
     IEnumerable<Transaction>? Transactions { get; set; } = new List<Transaction>();
 }
