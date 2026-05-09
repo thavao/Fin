8c792573b73d861b1f8aa562f0e0252bce1a6ab4
thavao <othavioact@gmail.com>
2026-05-08 21:29:49 -0300

    feat: adiciona controller de teste

---
+++ src/Fin.Api/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Fin.Domain.Entities;
+using Fin.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fin.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async void CreateUser()
+        {
+            var user = new User
+            {
+                Name = "Test",
+                Email = "email@email",
+                Password = "password",
+            };
+
+            await _userRepository.CreateUserAsync(user);
+        }
+
+    }
+}
